a016083155e7065e0169504f07c8e8d662d0c15b
jova <jova64kb@outlook.com>
2024-09-20 18:43:06 +0200

    port as optional argument added

--- server.py
+++ server.py
@@ -1,40 +1,81 @@
 import signal
 import sys
+import argparse
+import random
 import socket
 import select
 
 def sig_handler(sig, frame):
-    # close sock_listen ?
     print('exiting...')
     sys.exit(0)
 
 # map sig_handler
 signal.signal(signal.SIGINT, sig_handler)
 
-# CHECK FOR ERRORS!
+# args
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--port',
+                    type = int,
+                    help = 'specifies the port number to be used')
+args = parser.parse_args()
+port = 0
+lowest_port = 1024
+highest_port = 65535
+if args.port:
+    if args.port < lowest_port:
+        print('port lower than 1024')
+        sys.exit(1)
+    elif args.port > highest_port:
+        print('port greater than 65535')
+        sys.exit(1)
+    port = args.port
+else:
+    port = random.randint(lowest_port, highest_port)
 
 # getaddrinfo() -> returns a list of 5-tuples
-addr_info = socket.getaddrinfo(
-    '127.0.0.1',
-    8080,
-    socket.AF_INET,
-    socket.SOCK_STREAM,
-    socket.IPPROTO_TCP,
-    socket.AI_PASSIVE
-    )
+addr_info = None
+try:
+    addr_info = socket.getaddrinfo(
+        '127.0.0.1',
+        port,
+        socket.AF_INET,
+        socket.SOCK_STREAM,
+        socket.IPPROTO_TCP,
+        socket.AI_PASSIVE
+        )
+except OSError as err_msg:
+    print(f'could not get socket address information: ', err_msg)
+    sys.exit(1)
 
 # socket()
 af, socktype, proto, canonname, sa = addr_info[0]
-sock_listen = socket.socket(af, socktype, proto)
+sock_listen = None
+try:
+    sock_listen = socket.socket(af, socktype, proto)
+except OSError as err_msg:
+    print(f'socket could not be created: ', err_msg)
+    sys.exit(1)
 
 # setsockopt()
-sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+try:
+    sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+except OSError as err_msg:
+    print(f'socket options could not be set: ', err_msg)
+    sys.exit(1)
 
 # bind()
-sock_listen.bind(sa)
+try:
+    sock_listen.bind(sa)
+except OSError as err_msg:
+    print(f'socket could not be bound to port {port}: ', err_msg)
+    sys.exit(1)
 
 # listen()
-sock_listen.listen(10)
+try:
+    sock_listen.listen(10) # ??
+except OSError as err_msg:
+    print(f'server unable to accept connections: ', err_msg)
+    sys.exit(1)
 print(f'listening on: {sa[0]}:{sa[1]}')
 
 # select()
@@ -46,17 +87,17 @@ while True:
     for r in rlist:
         # new connection ready to be established
         if r == sock_listen:
+            # accept()
             sock_client, client_addr = sock_listen.accept()
             reads.append(sock_client)
             client_host, client_port = socket.getnameinfo(client_addr, socket.NI_NUMERICHOST)
             print(f'new connection established: {client_host}:{client_port}')
         # socket ready to be read from
         else:
-            data = r.recv(4096) # improve
+            data = r.recv(4096) # ??
             req_split = data.decode('utf-8').split('\r\n')
             close = False
             for s in req_split:
-                print(f'split: {s}')
                 if s == 'Connection: close':
                     close = True
             if close:
